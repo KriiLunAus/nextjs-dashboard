1622bf8a47510aba025fc77b7a70aa795dfee11d
KriiLunAus <146242487+KriiLunAus@users.noreply.github.com>
2024-11-19 14:04:21 +0200

    Learn Next.js Chapter 6

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Montserrat } from "next/font/google";
+import { Lusitana } from "next/font/google";
+
+export const lusitana = Lusitana({ subsets: ["latin"], weight: ["400"] });
+
+export const montserrat = Montserrat({ subsets: ["latin"] });
